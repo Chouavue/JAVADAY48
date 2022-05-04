4749e03b5f7fe74dec9373fdf48967c97ec1fc83
Choua Vue <vuec23@gmail.com>
2022-05-04 11:37:13 -0400

    first commit

---
+++ src/Casting.java
@@ -0,0 +1,22 @@
+
+public class Casting {
+        public static void main(String[] args) {
+                double num = 112.35;
+                System.out.println("Double: " + num);
+
+                int data = (int)num;
+                System.out.println("int: " + data);
+
+                String info = "49";
+                System.out.println("The string value is: "+ info);
+
+                int digit = Integer.parseInt(info);
+                System.out.println("The integar value is: " + digit);
+        }
+
+}
+
+
+
+
+
